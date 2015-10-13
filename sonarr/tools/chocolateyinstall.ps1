1790fb6e95c1372b5444fd474795f39e1c8c42ee
xenoputtss <xenoputtss@gmail.com>
2015-10-13 13:12:29 -0400

    Add Sonarr Chocolatey Package

---
+++ sonarr/tools/chocolateyinstall.ps1
@@ -0,0 +1,39 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'sonarr'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://download.sonarr.tv/v2/master/latest/NzbDrone.master.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  url           = $url
+
+  silentArgs    = "/verysilent"
+  validExitCodes= @(0, 3010, 1641)
+
+  softwareName  = 'sonarr*'
+  checksum      = '36caeef3c84a5044a3dbff5797ea64d9'
+  checksumType  = 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sonarr/tools/chocolateyuninstall.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop'; 
+
+$packageName = 'sonarr'
+$softwareName = 'Sonarr*' 
+$installerType = 'EXE' 
+
+$silentArgs = '/SILENT';
+$validExitCodes = @(0);
+
+$uninstalled = $false
+$local_key     = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$machine_key   = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
+
+$key = Get-ItemProperty -Path @($machine_key6432,$machine_key, $local_key) `
+                        -ErrorAction SilentlyContinue `
+         | ? { $_.DisplayName -like "$softwareName" }
+	 
+  $key | % { 
+    $file = "$($_.UninstallString)"
+    Uninstall-ChocolateyPackage -PackageName $packageName `
+                                -FileType $installerType `
+                                -SilentArgs "$silentArgs" `
+                                -ValidExitCodes $validExitCodes `
+                                -File "$file"
+}
