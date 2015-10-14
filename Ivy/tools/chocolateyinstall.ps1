48dbf6c43c3257a148a04f49a3d0c226ec3e4cb2
xenoputtss <xenoputtss@gmail.com>
2015-10-13 21:53:27 -0400

    Create a package for Apache-Ivy
    Initial setup, doesn't install to the path I want and needs to have java
    and ant setup as dependencies.

---
+++ Ivy/tools/chocolateyinstall.ps1
@@ -0,0 +1,28 @@
+﻿#create folder if not exists
+function CreateFolder ([string]$Path) {
+  New-Item -Path $Path -type directory -Force
+}
+ 
+$binRoot = Get-BinRoot
+ 
+CreateFolder($binRoot)
+ 
+$version = '2.4.0'
+$name = "apache-ivy-$version"
+$ivy_home = Join-Path $binRoot $name
+$ivy_bin = Join-Path $ivy_home 'bin'
+# $ivy_repo = Join-Path $env:USERPROFILE '.ivy'
+ 
+ #http://www.us.apache.org/dist//ant/ivy/2.4.0/apache-ivy-2.4.0-bin.zip
+$url = "http://www.us.apache.org/dist/ant/ivy/$version/$name-bin.zip"
+ 
+ 
+# [Environment]::SetEnvironmentVariable('IVY_HOME', $ivy_home, "User")
+# [Environment]::SetEnvironmentVariable('IVY', $ivy_bin, "User")
+# [Environment]::SetEnvironmentVariable('IVY_REPO', $ivy_repo, "User")
+ 
+Install-ChocolateyZipPackage 'Ivy' $url $binRoot #download the maven .zip and unzip in $binRoot folder
+ 
+# CreateFolder($ivy_repo)
+ 
+Install-ChocolateyPath $ivy_bin 'User' #add to path
---
+++ Ivy/tools/chocolateyuninstall.ps1
@@ -0,0 +1,23 @@
+﻿ 
+$version = '2.4.0'
+$name = "apache-ivy-$version"
+$binRoot = Get-BinRoot
+$ivy_home = Join-Path $binRoot $name
+$ivy_bin = Join-Path $ivy_home 'bin'
+# $ivy_repo = Join-Path $env:USERPROFILE '.ivy'
+ 
+# [Environment]::SetEnvironmentVariable('M2_HOME', $null, "User")
+# [Environment]::SetEnvironmentVariable('MAVEN_OPTS', $null, "User")
+# [Environment]::SetEnvironmentVariable('M2', $null, "User")
+# [Environment]::SetEnvironmentVariable('M2_REPO', $null, "User")
+ 
+"Please manually remove Maven ($m2_home) from the PATH environment variable."
+# Remove Maven from the path environment variable
+#if ($null -ne $env:path)
+#{
+#   $p = $env:path.Split(";") |? {$_.toLower() -ne $m2_bin}
+#    $newPath = [String]::Join(";")
+#    [Environment]::SetEnvironmentVariable('PATH', $newPath, "User")
+#}
+ 
+Remove-Item $ivy_home -Recurse -Force
